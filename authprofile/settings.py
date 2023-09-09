35563493dd2c14fc5195de856aa19752ba5579c5
Nkechi Elechi <elechinkechi9@gmail.com>
2023-09-09 12:38:25 +0100

    Changed aloowable host to allow all hosts

--- authprofile/settings.py
+++ authprofile/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v)fuiujlclv&3_$s*p@iuwdw8+)p5(@086mx(jxwuz@rzt^^-o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
